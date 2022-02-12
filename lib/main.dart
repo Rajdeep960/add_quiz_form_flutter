80bc8eb1cb1002dcd28e85ebbbeb1e87c6c9aa9d
Rajdeep Mondal <84460439+Rajdeep960@users.noreply.github.com>
2022-02-12 18:21:00 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/screens/create_quiz.dart';
+import 'package:quiz_app/screens/select_quiz.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CreateQuiz(),
+      home: SelectQuiz(),
     );
   }
 }
